af268e53e99677041efcdc078db0e798f2ae8109
MadaoDemo <1429544545@qq.com>
2018-12-04 18:44:29 +0800

    ajax3.0

--- routes/demo.js
+++ routes/demo.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const pool = require('../pool');
 const router = express();
+
+function validate(body) {
+    var mystr = '';
+    var mycode = 400
+    for (const key in body) {
+        if (body[key] == '') {
+            mystr = `${key} is null !!`;
+            break;
+        }
+        mycode ++;
+    }
+    return { msg: mystr, code: mycode };
+}
 
 router.get('/ajaxDemo', (req, res) => {
 
     res.send('这是第一个ajax');
 });
 
-router.get('/login', (req, res) => {
+router.get('/demologin', (req, res) => {
     var uname_ = req.query.uname;
     var upwd_ = req.query.upwd;
 
@@ -24,7 +38,73 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/postAjax', (req, res) => {
-    res.send(req.body);
+    var obj = req.body;
+    var validateResule = validate(obj);
+    if (validateResule.msg) {
+        res.send({code: validateResule.code, msg: validateResule.msg});
+        return;
+    }
+    var sql = 'SELECT * FROM xz_user WHERE uname = ? AND upwd = ?';
+    pool.query(sql, [obj.uname, obj.upwd], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        if (result.length) {
+            res.send({code: 200, msg: 'Login Success!!'});
+        } else {
+            res.send({code: 401, msg: 'Login Error!!'});
+        }
+    });
+});
+
+router.post('/login', (req, res) => {
+    var obj = req.body;
+    var validateResule = validate(obj);
+    if (validateResule.msg) {
+        res.send({code: validateResule.code, msg: validateResule.msg});
+        return;
+    }
+    var sql = 'INSERT INTO xz_user SET ?';
+
+    pool.query(sql, [obj], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        if (result.affectedRows) {
+            res.send({code: 200, msg: 'Insert Success !!'});
+        } else {
+            res.send({code: 401, msg: 'Insert Error !!'});
+        }
+    })
+});
+
+router.get('/getlist', (req, res) => {
+    var sql = 'SELECT * FROM xz_user';
+    pool.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        if (result.length) {
+            res.send(result);
+        } else {
+            res.send('Error!!!');
+        }
+    });
+});
+
+router.post('/validate', (req, res) => {
+    var obj = req.body;
+    var sql = 'SELECT * FROM xz_user WHERE uname = ?';
+    pool.query(sql, [obj.uname], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        if (result.length) {
+            res.send('用户名被占用');
+        } else {
+            res.send('用户名可用');
+        }
+    });
 });
 
 
